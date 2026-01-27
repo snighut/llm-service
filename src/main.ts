ca426f26fc48a13c8f91c2fddf943dbd5941ed0f
Swapnil Nighut <sbnighut@gmail.com>
2026-01-27 01:02:37 -0700

    fix: removing dupe listning

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
-  await app.listen(3002, '0.0.0.0'); // <--- Add '0.0.0.0' here
 
   // Security
   app.use(helmet());
@@ -43,7 +42,7 @@ async function bootstrap() {
   app.enableShutdownHooks();
 
   const port = process.env.PORT || 3002;
-  // await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Application is running on: http://localhost:${port}/api/v1`);
   console.log(`📚 API Documentation: http://localhost:${port}/api/docs`);
